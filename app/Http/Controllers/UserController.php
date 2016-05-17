754e314f50420fd1f8ef5f86db404f75a0a88bb6
krish <krish.sumaroo@gmail.com>
2016-05-17 15:30:19 -0400

    user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+public function __construct()
+{
+    //
+}
+
+//
+
+public function register()
+{
+
+    //need to validate
+    //use middleware
+
+    $user = new User();
+    
+}
+
+
+}
